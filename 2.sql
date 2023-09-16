96247c9cc89073a27c8d26fcfac4284da7e7b4af
Ignacio Riquelme <ignacio.riquelme.r@usach.cl>
2023-09-15 22:40:13 -0300

    Cambios menores

--- 2.sql
+++ 2.sql
@@ -1,6 +1,6 @@
-SELECT ID_Edificio, MONTH(p.Fecha_Emision) AS Mes, SUM(p.Monto) AS Recaudo
+SELECT e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Emision) AS Mes, SUM(p.Monto) AS Recaudo
 FROM Edificio e
 JOIN Depto d ON e.ID_Edificio = d.ID_Edificio
 JOIN PAGO_GASTO_DEPTO p ON d.ID_depto = p.ID_depto
-GROUP BY e.ID_Edificio, MONTH(p.Fecha_Emision)
-ORDER BY e.ID_Edificio, MONTH(p.Fecha_Emision) 
+GROUP BY e.ID_Edificio, p.Fecha_Emision
+ORDER BY e.ID_Edificio, p.Fecha_Emision
