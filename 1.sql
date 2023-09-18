5b00f1d554789d7f1cc99eb6f1f0ce13ba3f1fe6
Ignacio Riquelme <ignacio.riquelme.r@usach.cl>
2023-09-18 19:41:25 -0300

    Correcion sentencia 1

--- 1.sql
+++ 1.sql
@@ -1,5 +1,7 @@
-SELECT ID_depto
-FROM Depto
-WHERE ID_depto NOT IN (SELECT ID_depto 
-                                FROM PAGO_GASTO_DEPTO 
-                                WHERE Fecha_Limite >  CURRENT_DATE - INTERVAL '3 months')
+SELECT e.ID_Edificio AS Edificio, d.ID_depto AS Depto, p.Fecha_Limite, p.Fecha_Pago
+FROM Edificio e
+JOIN Depto d ON e.ID_Edificio = d.ID_Edificio
+JOIN PAGO_GASTO_DEPTO p on d.ID_depto = p.ID_depto
+WHERE p.Fecha_Emision >= (CURRENT_DATE - INTERVAL '3 months') AND (p.Fecha_Pago > p.Fecha_Limite OR p.Fecha_Pago IS NULL)
+GROUP BY e.ID_Edificio, d.ID_depto,  p.Fecha_Limite, p.Fecha_Pago
+ORDER BY e.ID_Edificio, p.Fecha_Limite
