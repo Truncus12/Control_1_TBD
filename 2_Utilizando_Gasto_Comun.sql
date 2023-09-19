2d4231d761c5fb0ad7b2c5a31f0a16785c6ae0ac
Ignacio Riquelme <ignacio.riquelme.r@usach.cl>
2023-09-19 01:24:14 -0300

    p.Fecha_Pago IS NOT NULL

--- 2_Utilizando_Gasto_Comun.sql
+++ 2_Utilizando_Gasto_Comun.sql
@@ -1,7 +1,7 @@
-SELECT e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Emision) AS Mes, SUM(g.mantencion + g.servicios) AS Recaudo
+SELECT e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Pago) AS Mes, SUM(g.mantencion + g.servicios) AS Recaudo
 FROM Edificio e
 JOIN Depto d ON e.ID_Edificio = d.ID_Edificio
-JOIN PAGO_GASTO_DEPTO p ON d.ID_depto = p.ID_depto
+JOIN PAGO_GASTO_DEPTO p ON d.ID_depto = p.ID_depto AND p.Fecha_Pago IS NOT NULL
 JOIN GASTO_COMUN g ON g.ID_Gasto_Comun = p.ID_Gasto_Comun
-GROUP BY e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Emision)
-ORDER BY e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Emision);
+GROUP BY e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Pago)
+ORDER BY e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Pago);
