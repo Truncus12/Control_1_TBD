e14a50a5de75d36afd179ef716157269497aa572
Angel Uribe <angel.uribe.p@usach.cl>
2023-09-19 18:19:56 -0300

    Se movieron las sentencias a otra carpeta y se añade diccionario

--- 6.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    Administrador.Nombre AS Nombre,
-    COUNT(Edificio.ID_Edificio) AS Cantidad_Edificios
-FROM Administrador
-LEFT JOIN Edificio
-    ON Edificio.ID_Administrador = Administrador.ID_Administrador
-GROUP BY Administrador.ID_Administrador
-ORDER BY Cantidad_Edificios DESC
