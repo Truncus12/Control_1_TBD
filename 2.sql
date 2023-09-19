5db70beb767ed4b0760bae70dc601f4c6013d932
Ignacio Riquelme <ignacio.riquelme.r@usach.cl>
2023-09-19 01:36:13 -0300

    p.Fecha_Pago IS NOT NULL

--- 2.sql
+++ 2.sql
@@ -1,6 +1,6 @@
-SELECT e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Emision) AS Mes, SUM(p.Monto) AS Recaudo
+SELECT e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Pago) AS Mes, SUM(p.Monto) AS Recaudo
 FROM Edificio e
 JOIN Depto d ON e.ID_Edificio = d.ID_Edificio
-JOIN PAGO_GASTO_DEPTO p ON d.ID_depto = p.ID_depto
-GROUP BY e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Emision)
-ORDER BY e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Emision)
+JOIN PAGO_GASTO_DEPTO p ON d.ID_depto = p.ID_depto AND p.Fecha_Pago IS NOT NULL
+GROUP BY e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Pago)
+ORDER BY e.ID_Edificio, EXTRACT(MONTH FROM p.Fecha_Pago)
